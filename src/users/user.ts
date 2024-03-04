e6f760014cc78726e98f11f6d1b6749b1994b6f8
Tyui70 <118425755+Tyui70@users.noreply.github.com>
2024-03-04 10:09:07 +0100

    Update user.ts

--- src/users/user.ts
+++ src/users/user.ts
@@ -1,6 +1,11 @@
 import bodyParser from "body-parser";
 import express from "express";
 import { BASE_USER_PORT } from "../config";
+import crypto from 'crypto';
+
+
+let lastReceivedMessage: string | null = null;
+let lastSentMessage: string | null = null;
 
 export type SendMessageBody = {
   message: string;
@@ -13,7 +18,26 @@ export async function user(userId: number) {
   _user.use(bodyParser.json());
 
   // TODO implement the status route
-  // _user.get("/status", (req, res) => {});
+   _user.get("/status", (req, res) => { res.send('live');});
+
+   _user.get('/getLastReceivedMessage', (req, res) => {
+    res.json({ result : lastReceivedMessage });
+});
+
+// Route to get the last sent message
+  _user.get('/getLastSentMessage', (req, res) => {
+    res.json({ result: lastSentMessage });
+});
+
+_user.post("/message", (req, res) => {
+  const { message }: { message: string } = req.body;
+
+  // Update the last received message
+  lastReceivedMessage = message;
+  
+ res.send('success');
+  //res.json({ message: "Message received successfully" });
+});
 
   const server = _user.listen(BASE_USER_PORT + userId, () => {
     console.log(
