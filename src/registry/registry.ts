21e55a980e61ba44991a568c93e75570ca108076
Tyui70 <118425755+Tyui70@users.noreply.github.com>
2024-03-04 10:52:38 +0100

    Update registry.ts

--- src/registry/registry.ts
+++ src/registry/registry.ts
@@ -2,7 +2,7 @@ import bodyParser from "body-parser";
 import express, { Request, Response } from "express";
 import { REGISTRY_PORT } from "../config";
 
-export type Node = { nodeId: number; pubKey: string; privateKey?: string };
+export type Node = { nodeId: number; pubKey: string };
 
 export type RegisterNodeBody = {
   nodeId: number;
@@ -10,48 +10,38 @@ export type RegisterNodeBody = {
 };
 
 export type GetNodeRegistryBody = {
-  nodes: (Node & { privateKey?: string })[];
+  nodes: Node[];
 };
 
-const registeredNodes: Node[] = [];
-
 export async function launchRegistry() {
+  let getNodeRegistryBody: GetNodeRegistryBody = { nodes: [] };
+
   const _registry = express();
   _registry.use(express.json());
   _registry.use(bodyParser.json());
 
-  _registry.post('/registerNode', (req, res) => {
-    const { nodeId, pubKey }: RegisterNodeBody = req.body;
-
-    // Check if the node is already registered
-    const existingNode = registeredNodes.find((node) => node.nodeId === nodeId);
-
-    if (existingNode) {
-      return res.status(400).json({ message: `Node ${nodeId} is already registered.` });
-    }
-
-
-    // Add the node to the registered nodes array
-    registeredNodes.push({ nodeId, pubKey});
-    const nodeRegistry: GetNodeRegistryBody = { nodes: registeredNodes };
-    res.json(nodeRegistry);
-
-    return res.status(201).json({ message: `Node ${nodeId} successfully registered.` });
+  // 1.3 implement the status route
+  _registry.get("/status", (req, res) => {
+    res.send("live");
   });
 
-  _registry.get('/getNodeRegistry', (req, res) => {
-    const nodeRegistry: GetNodeRegistryBody = { nodes: registeredNodes };
-    res.json(nodeRegistry);
+  // 3.1 allow nodes to register themselves
+  // /registerNode
+  _registry.post("/registerNode", (req: Request<RegisterNodeBody>, res: Response) => {
+    const { nodeId, pubKey } = req.body;
+    getNodeRegistryBody.nodes.push({ nodeId, pubKey });
+    res.json({ result: "success" });
   });
 
-  
-
-  // TODO implement the status route
-   _registry.get("/status", (req, res) => { res.send('live');});
+  // 3.4 allow users to retrieve the registry
+  // /getNodeRegistry
+  _registry.get("/getNodeRegistry", (req, res) => {
+    res.json(getNodeRegistryBody);
+  });
 
   const server = _registry.listen(REGISTRY_PORT, () => {
     console.log(`registry is listening on port ${REGISTRY_PORT}`);
   });
 
   return server;
-} 
+}
